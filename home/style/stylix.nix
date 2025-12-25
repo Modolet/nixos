7965f0075137a9cef5cf0a1f93b5890705a9b609
modolet <y@xxyx.io>
2025-12-25 23:45:08 +0800

    feat: 为 stylix 图标主题添加上色脚本

--- home/style/stylix.nix
+++ home/style/stylix.nix
@@ -1,4 +1,32 @@
-{ lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  stylixColors = config.lib.stylix.colors.withHashtag;
+  palette =
+    [
+      stylixColors.base00
+      stylixColors.base01
+      stylixColors.base02
+      stylixColors.base03
+      stylixColors.base04
+      stylixColors.base05
+      stylixColors.base06
+      stylixColors.base07
+      stylixColors.base08
+      stylixColors.base09
+      stylixColors.base0A
+      stylixColors.base0B
+      stylixColors.base0C
+      stylixColors.base0D
+      stylixColors.base0E
+      stylixColors.base0F
+    ];
+  paletteArg = lib.concatStringsSep "," palette;
+  recolorScript = ''
+    ${pkgs.python3.withPackages (ps: with ps; [ pillow tqdm ])}/bin/python ${../../tools/icon-recolor.py} \
+      --palette '${paletteArg}' \
+      --src "$out/share/icons"
+  '';
+in {
   stylix = {
     enable = true;
     autoEnable = false;
@@ -30,10 +58,9 @@
     };
     iconTheme = {
       enable = true;
-      # package = pkgs.zafiro-icons.overrideAttrs (oldAttrs: {
-      #   postInstall = recolorScript + (oldAttrs.postInstall or "");
-      # });
-      package = pkgs.zafiro-icons;
+      package = pkgs.zafiro-icons.overrideAttrs (oldAttrs: {
+        postInstall = recolorScript + (oldAttrs.postInstall or "");
+      });
       dark = "Zafiro-icons-Dark";
       light = "Zafiro-icons-Light";
     };
