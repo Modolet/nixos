5eb8920aa2cac65891c3d0355cdf4543dcde4d25
modolet <y@xxyx.io>
2026-01-09 01:27:09 +0800

    fix: 纠正 theme-switch 特化遍历变量

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -72,7 +72,7 @@
       if [ -z "$specialisations_dir" ]; then
         if [ -d "$profile_dir" ]; then
           for num in $(ls "$profile_dir"/home-manager-*-link 2>/dev/null | sed 's#.*/home-manager-##; s#-link##' | sort -nr); do
-            candidate="$profile_dir/home-manager-${num}-link"
+            candidate="$profile_dir/home-manager-$num-link"
             target_link="$(readlink -f "$candidate" || true)"
             if [ -n "$target_link" ] && [ -d "$target_link/specialisation" ]; then
               base_dir="$target_link"
