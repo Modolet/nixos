97fae13acd31a720f4eaf6835afce5f47dac9d9a
modolet <y@xxyx.io>
2026-01-18 02:44:40 +0800

    fix: 修正 niri systemd 服务替换路径

--- pkgs/overlays/default.nix
+++ pkgs/overlays/default.nix
@@ -1,5 +1,7 @@
 { inputs }:
 final: prev:
 {
-  niri = inputs.niri.packages.${prev.system}.niri-unstable;
+  niri = inputs.niri.packages.${prev.system}.niri-unstable.override {
+    replace-service-with-usr-bin = true;
+  };
 }
