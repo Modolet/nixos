3f6780c9d7d7e1ee6cc54db8c049ccdd00c6b1db
modolet <y@xxyx.io>
2026-01-17 17:22:48 +0800

    refactor: win-clang使用xwin生成sysroot包

--- devshells/win-clang.nix
+++ devshells/win-clang.nix
@@ -12,52 +12,46 @@
         ninja
         pkg-config
       ];
+      winSysroot = pkgs.stdenvNoCC.mkDerivation {
+        pname = "xwin-sysroot";
+        version = pkgs.xwin.version;
+        nativeBuildInputs = [ pkgs.xwin ];
+        dontUnpack = true;
+        __noChroot = true;
+        preferLocalBuild = true;
+        allowSubstitutes = false;
+        installPhase = ''
+          export XWIN_ACCEPT_LICENSE=1
+          export XWIN_CACHE_DIR="$TMPDIR/xwin-cache"
+          mkdir -p "$XWIN_CACHE_DIR"
+
+          xwin --accept-license \
+            --cache-dir "$XWIN_CACHE_DIR" \
+            --arch x86 --arch x86_64 \
+            --variant desktop \
+            --include-debug-runtime \
+            download
+
+          xwin --accept-license \
+            --cache-dir "$XWIN_CACHE_DIR" \
+            --arch x86 --arch x86_64 \
+            --variant desktop \
+            splat \
+            --output "$out" \
+            --use-winsysroot-style \
+            --include-debug-libs
+        '';
+      };
     in
     {
       devShells.win-clang = pkgs.mkShell {
-        packages = winPackages;
-        shellHook = ''
-          export XWIN_CACHE_DIR="''${XWIN_CACHE_DIR:-$HOME/.cache/xwin}"
-          export XWIN_SYSROOT="''${XWIN_SYSROOT:-$HOME/.local/share/xwin/winsysroot}"
-          export XWIN_ARCHS="''${XWIN_ARCHS:-x86 x86_64}"
-          export XWIN_VARIANT="''${XWIN_VARIANT:-desktop}"
-
-          export CC=clang-cl
-          export CXX=clang-cl
-          export LD=lld-link
-          export AR=llvm-lib
-          export RC=llvm-rc
-
-          xwin-fetch() {
-            xwin --accept-license \
-              --cache-dir "$XWIN_CACHE_DIR" \
-              --arch x86 --arch x86_64 \
-              --variant "$XWIN_VARIANT" \
-              --include-debug-runtime \
-              download
-
-            xwin --accept-license \
-              --cache-dir "$XWIN_CACHE_DIR" \
-              --arch x86 --arch x86_64 \
-              --variant "$XWIN_VARIANT" \
-              splat \
-              --output "$XWIN_SYSROOT" \
-              --use-winsysroot-style \
-              --include-debug-libs
-          }
-
-          use-win32() {
-            export WIN_TARGET="i686-pc-windows-msvc"
-            export CFLAGS="--target=$WIN_TARGET --winsysroot=$XWIN_SYSROOT"
-            export CXXFLAGS="$CFLAGS"
-          }
-
-          use-win64() {
-            export WIN_TARGET="x86_64-pc-windows-msvc"
-            export CFLAGS="--target=$WIN_TARGET --winsysroot=$XWIN_SYSROOT"
-            export CXXFLAGS="$CFLAGS"
-          }
-        '';
+        packages = winPackages ++ [ winSysroot ];
+        XWIN_SYSROOT = "${winSysroot}";
+        CC = "clang-cl";
+        CXX = "clang-cl";
+        LD = "lld-link";
+        AR = "llvm-lib";
+        RC = "llvm-rc";
       };
     };
 }
