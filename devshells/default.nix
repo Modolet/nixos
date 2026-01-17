0b7f694cd1aa38557d0a60e1169073d8ca68932c
modolet <y@xxyx.io>
2026-01-17 16:23:29 +0800

    fix: 补全Rust运行库路径

--- devshells/default.nix
+++ devshells/default.nix
@@ -3,24 +3,33 @@
   perSystem =
     { pkgs, ... }:
     {
-      devShells.rust = pkgs.mkShell {
-        packages = with pkgs; [
-          rustc
-          cargo
-          rustfmt
-          clippy
-          rust-analyzer
-          pkg-config
-          systemd.dev
-          wayland
-          wayland-protocols
-          libxkbcommon
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-        ];
-      };
+      devShells.rust =
+        let
+          rustPackages = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+            pkg-config
+            systemd.dev
+            wayland
+            wayland-protocols
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            xorg.libxcb
+            vulkan-loader
+          ];
+        in
+        pkgs.mkShell {
+          packages = rustPackages;
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath rustPackages}:$LD_LIBRARY_PATH"
+          '';
+        };
       devShells.clang = pkgs.mkShell {
         packages = with pkgs; [
           clang
