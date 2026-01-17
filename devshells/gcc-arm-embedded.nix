b649925c4e33428ae431c6dc4ed9b95930720984
modolet <y@xxyx.io>
2026-01-17 20:50:16 +0800

    fix: gcc-arm-embedded添加clangd驱动

--- devshells/gcc-arm-embedded.nix
+++ devshells/gcc-arm-embedded.nix
@@ -1,12 +1,30 @@
 _: {
   perSystem =
     { pkgs, ... }:
+    let
+      llvmPkgs =
+        if pkgs ? llvmPackages_21 then
+          pkgs.llvmPackages_21
+        else
+          pkgs.llvmPackages;
+      clangdWrapper = pkgs.runCommand "clangd-gcc-arm-embedded" { } ''
+        mkdir -p "$out/bin"
+        cat > "$out/bin/clangd" <<'EOF'
+        #!${pkgs.runtimeShell}
+        exec ${llvmPkgs.clang-tools}/bin/clangd \
+          --query-driver="${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-*" \
+          "$@"
+        EOF
+        chmod +x "$out/bin/clangd"
+      '';
+    in
     {
       devShells.gcc-arm-embedded = pkgs.mkShell {
-        packages = with pkgs; [
-          gcc-arm-embedded
-          cmake
-          ninja
+        packages = [
+          pkgs.gcc-arm-embedded
+          pkgs.cmake
+          pkgs.ninja
+          clangdWrapper
         ];
       };
     };
