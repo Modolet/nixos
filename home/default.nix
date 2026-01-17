00a4bafd221fbcc1f41fcef4501818e5d7f21346
modolet <y@xxyx.io>
2026-01-17 15:36:42 +0800

    fix: 固定XDG用户目录为英文

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   imports = [
     ./editor/neovim
@@ -48,6 +48,19 @@
     packages = with pkgs; [
       fastfetch
     ];
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "${config.home.homeDirectory}/Desktop";
+    documents = "${config.home.homeDirectory}/Documents";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/Music";
+    pictures = "${config.home.homeDirectory}/Pictures";
+    publicShare = "${config.home.homeDirectory}/Public";
+    templates = "${config.home.homeDirectory}/Templates";
+    videos = "${config.home.homeDirectory}/Videos";
   };
 
   desktopShell = "dms";
