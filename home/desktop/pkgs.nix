8287146194a28046883a8e12e3690d4261a31e06
modolet <y@xxyx.io>
2026-01-18 01:26:55 +0800

    feat: 添加 winapps 启动器

--- home/desktop/pkgs.nix
+++ home/desktop/pkgs.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  inputs,
+  ...
+}:
 {
   home.packages = with pkgs; [
     nautilus
     seafile-client
     evolution
     wineWowPackages.waylandFull
+    xsettingsd
+    inputs.winapps.packages.${pkgs.system}.winapps
+    inputs.winapps.packages.${pkgs.system}.winapps-launcher
   ];
 
 }
