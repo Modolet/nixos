8dc4ce33826e06aac5440e4f783279f2262d9df8
modolet <y@xxyx.io>
2026-01-17 20:55:08 +0800

    refactor: gcc-arm-embedded使用头文件环境变量

--- devshells/gcc-arm-embedded.nix
+++ devshells/gcc-arm-embedded.nix
@@ -2,30 +2,34 @@ _: {
   perSystem =
     { pkgs, ... }:
     let
-      llvmPkgs =
-        if pkgs ? llvmPackages_21 then
-          pkgs.llvmPackages_21
-        else
-          pkgs.llvmPackages;
-      clangdWrapper = pkgs.runCommand "clangd-gcc-arm-embedded" { } ''
-        mkdir -p "$out/bin"
-        cat > "$out/bin/clangd" <<'EOF'
-        #!${pkgs.runtimeShell}
-        exec ${llvmPkgs.clang-tools}/bin/clangd \
-          --query-driver="${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-*" \
-          "$@"
-        EOF
-        chmod +x "$out/bin/clangd"
-      '';
+      gccArm = pkgs.gcc-arm-embedded;
+      gccVersion = gccArm.version;
+      sysroot = "${gccArm}/arm-none-eabi";
+      gccLib = "${gccArm}/lib/gcc/arm-none-eabi/${gccVersion}";
+      cIncludes = [
+        "${sysroot}/include"
+        "${gccLib}/include"
+        "${gccLib}/include-fixed"
+      ];
+      cxxIncludes = [
+        "${sysroot}/include/c++/${gccVersion}"
+        "${sysroot}/include/c++/${gccVersion}/arm-none-eabi"
+        "${sysroot}/include/c++/${gccVersion}/backward"
+      ];
+      cIncludePath = pkgs.lib.concatStringsSep ":" cIncludes;
+      cxxIncludePath = pkgs.lib.concatStringsSep ":" (cxxIncludes ++ cIncludes);
     in
     {
       devShells.gcc-arm-embedded = pkgs.mkShell {
         packages = [
-          pkgs.gcc-arm-embedded
+          gccArm
           pkgs.cmake
           pkgs.ninja
-          clangdWrapper
         ];
+        ARM_NONE_EABI_SYSROOT = sysroot;
+        C_INCLUDE_PATH = cIncludePath;
+        CPLUS_INCLUDE_PATH = cxxIncludePath;
+        CPATH = cxxIncludePath;
       };
     };
 }
