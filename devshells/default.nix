c67c91eb667094538f79910c173d67a60d70a443
modolet <y@xxyx.io>
2026-01-17 16:31:55 +0800

    feat: 添加Qt5开发shell

--- devshells/default.nix
+++ devshells/default.nix
@@ -44,5 +44,17 @@
           pkg-config
         ];
       };
+      devShells.qt5 = pkgs.mkShell {
+        packages = with pkgs; [
+          qt5.qtbase
+          qt5.qttools
+          qt5.qtsvg
+          qt5.qtdeclarative
+          qt5.qtwayland
+          cmake
+          ninja
+          pkg-config
+        ];
+      };
     };
 }
